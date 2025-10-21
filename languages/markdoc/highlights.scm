ee06897794dc21041cea37cb1f70f3a1bc947cd3
Shelton Louis <louisshelton0@gmail.com>
2025-10-21 11:55:59 -0400

    feat(highlights): add comprehensive highlighting for lists and variables
    Added highlighting for:
    - Variables with $ prefix: (variable) @variable.special
    - Inline expressions: (inline_expression) @embedded for {{ ... }}
    - Lists: (list_marker) @punctuation.list_marker for -, *, +, 1.
    - List items and paragraphs: (list_item), (list_paragraph)
    - Boolean and null literals: (boolean), (null)
    - All binary and unary operators from grammar
    - Enhanced expressions: call_expression, member_expression, array_access
    - Inline tags: (inline_tag) for inline {% tag /%}
    
    This should now properly highlight:
    - {{ .name }} variables
    - - List items with markers
    - All Markdoc syntax elements

--- languages/markdoc/highlights.scm
+++ languages/markdoc/highlights.scm
@@ -3,8 +3,16 @@
 (identifier) @variable
 (number) @number
 (string) @string
+(boolean) @boolean
+(null) @constant
 
-; Headings (from grammar: heading, heading_marker, heading_text)
+; Variables (with $ prefix)
+(variable) @variable.special
+
+; Inline expressions ({{ ... }})
+(inline_expression) @embedded
+
+; Headings
 (heading_text) @title
 (heading_marker) @punctuation.special
 
@@ -12,25 +20,71 @@
 (fenced_code_block) @text.literal
 (code) @text.literal
 (language) @attribute
+(attributes) @attribute
+
+; Lists
+(list) @text
+(list_item) @text
+(list_marker) @punctuation.list_marker
+(list_paragraph) @text
 
 ; Markdoc tags
 (tag_name) @tag
 (attribute_name) @property
+(attribute_value) @string
+(inline_tag) @tag
 
 ; Text formatting
 (emphasis) @emphasis
 (strong) @emphasis.strong
 (inline_code) @string.special
 
-; Links
+; Links and images
+(link) @text
 (link_text) @link_text
 (link_destination) @link_uri
+(image) @text
+(image_alt) @link_text
+(image_destination) @link_uri
 
 ; HTML
 (html_comment) @comment
+(html_block) @embedded
+(html_inline) @embedded
+
+; Expressions
+(call_expression) @function
+(member_expression) @variable
+(array_access) @variable
+(array_literal) @punctuation.bracket
+(object_literal) @punctuation.bracket
+
+; Operators
+(binary_add) @operator
+(binary_subtract) @operator
+(binary_multiply) @operator
+(binary_divide) @operator
+(binary_equal) @operator
+(binary_not_equal) @operator
+(binary_and) @operator
+(binary_or) @operator
+(unary_not) @operator
+(unary_minus) @operator
+(unary_plus) @operator
 
 ; Punctuation
 "{%" @punctuation.delimiter
 "%}" @punctuation.delimiter
 "{{" @punctuation.delimiter
 "}}" @punctuation.delimiter
+"/%}" @punctuation.delimiter
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+"=" @operator
+"." @operator
+"," @punctuation.delimiter
+":" @punctuation.delimiter
